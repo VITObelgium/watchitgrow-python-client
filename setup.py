2edb2b2aa1567557675e051a29b40d97f66eec91
janssenb <bram.janssen@vito.be>
2022-09-29 13:23:24 +0200

    build: updated setup file

--- setup.py
+++ setup.py
@@ -13,16 +13,19 @@ setup(name=name,
       description='Python Client API for WatchItGrow',
       url="https://watchitgrow.be",
       python_requires=">=3.6",
-      packages=find_packages(include=['wig*']),
-      include_package_data=True,
+      packages=find_packages(),
       tests_require=tests_require,
       test_suite='tests',
       install_requires=[
-          'requests>=2.26.0',
+          'requests',
           'oauthlib',
           'requests_oauthlib',
           'shapely'
       ],
+      include_package_data=True,
+      package_data={
+          "": ["**/*.conf"],
+      },
       extras_require={
           "dev": tests_require + []
       },
